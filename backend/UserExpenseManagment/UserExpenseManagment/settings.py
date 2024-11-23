b8e4f3f49d119d0876837d58792291ddd676844c
Munib <139039102+Munib78@users.noreply.github.com>
2024-11-23 23:02:44 +0530

    Update settings.py

--- backend/UserExpenseManagment/UserExpenseManagment/settings.py
+++ backend/UserExpenseManagment/UserExpenseManagment/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userManagment',
-    'Expenses',
     'phonenumber_field',
     'rest_framework',
     'corsheaders'
